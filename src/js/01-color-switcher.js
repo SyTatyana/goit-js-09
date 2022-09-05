4deb59a488a83fe14f9336a5d1e7696064cafd56
Tatyana Syrbu <tanja.karamilja@gmail.com>
2022-09-05 19:24:45 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timeId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', changingColor);
+function changingColor(e) {
+  timeId = setInterval(() => {
+    console.log(timeId);
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  if (timeId) {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }
+}
+
+stopBtn.addEventListener('click', stopChangingColor);
+
+function stopChangingColor(e) {
+  clearInterval(timeId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  stopBtn.style.backgroundColor = '';
+}
